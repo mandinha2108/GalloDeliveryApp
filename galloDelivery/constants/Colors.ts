bb635515c453fd04392209d2762fe4269a2a5516
OrtigosaLaura <lauraortigosa378@gmail.com>
2025-08-25 07:58:23 -0300

    aaaaaaaaaa

---
+++ galloDelivery/constants/Colors.ts
@@ -0,0 +1,28 @@
+import { Background } from "@react-navigation/elements";
+
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#ffffff';
+
+export default {
+    light: {
+        text: '',
+        background: '',
+        tint: tintColorLight,
+        tabIconDefault: '#cccccc',
+        tabIconSelected: tintColorLight
+    },
+    dark: {
+        text: '#ffffff',
+        background: '#000000',
+        tint: tintColorLight,
+        tabIconDefault: '#cccccc',
+        tabIconSelected: tintColorLight
+    },
+    primary:'20E182',
+    lighGrey: '#FCF8FF',
+    grey: 'EEE9F0',
+    medium: '9F9AA1',
+    mediumDark: '#424242',
+    green: '#437919',
+
+}
