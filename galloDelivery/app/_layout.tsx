089003e12ec922f4fbe6423d15e47217390073ee
mandinha2108 <amafur8@gmail.com>
2025-09-11 11:31:31 -0300

    hhgggggggggggggggggggggggggggg

--- galloDelivery/app/_layout.tsx
+++ galloDelivery/app/_layout.tsx
@@ -1,14 +1,23 @@
 import { Stack } from "expo-router";
 import CustomHeader from "@/components/CustomHeader";
+import { BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return ( 
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <BottomSheetModalProvider> 
+    
   <Stack>
     <Stack.Screen
     name="index"
     options={{ header: () => <CustomHeader /> }}
     />
   </Stack>
+
+  </BottomSheetModalProvider>
+</GestureHandlerRootView>
   );
 }
+
 
